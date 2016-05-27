f2b9720394b40011634b86beafe8e158c1d46932
D2CPoritekuFukuyama <d2c_poriteku@yahoo.co.jp>
2016-05-28 01:27:15 +0900

    first commit

---
+++ dotfilesLink.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -sf /home/pi/dotfiles/vimrc /etc/vim/vimrc
+ln -sf /home/pi/dotfiles/apt.conf /etc/apt/apt.conf
+ln -sf /home/pi/dotfiles/dhcpcd.conf /etc/dhcpcd.conf
+ln -sf /home/pi/dotfiles/.bashrc /home/pi/.bashrc
+ln -sf /home/pi/dotfiles/smb.conf /etc/samba/smb.conf
